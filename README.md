a32112a04a5d9ded9ce9d33c7cfd00a5863c420b
Chris Rad <raddatzchris@gmail.com>
2024-03-22 14:51:27 -0500

    New visual and editing Lm

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # ISYE-6740-
 Repo for final project
+
+
+# Project Timeline
+
+1) First we will load the data and combine the two dataframes, while cleaning out states that have many null values and cleaning up the date format. 
+
+2) Secondly, we can do some data exploration with histograms, barcharts, correlation heatmaps, any sort of visual representation of the data. 
+
+3) First I want to try building models out of the original data, but we can then look into performing PCA and finding some similarities across that as well. 
--- linear regression.py
+++ linear regression.py
@@ -1,18 +1,32 @@
 from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import GridSearchCV, train_test_split
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+np.random.seed(21)
 
 file = pd.read_csv("merged_df.csv")
-# print(file.head())
 
 states = list(file.columns)[4:]
 
-model = LinearRegression()
+model = LinearRegression(fit_intercept= False, n_jobs = -1)
 x = file[['CPI']]
 y = file[states]
 
+xtrain, xtest, ytrain, ytest = train_test_split(x, y, test_size = 0.15, train_size = .85, random_state = 21)
+model.fit(xtrain, ytrain)
 
-model.fit(x, y)
-
-print(model.intercept_)
+# print(model.intercept_) #Have an intercept of 0
 cdf = pd.DataFrame(model.coef_, states, columns=['Coefficients'])
 print(cdf)
+
+sns.barplot(cdf.T)
+plt.xticks(rotation = 90)
+plt.title("State coefficients for Regressing on CPI")
+plt.xlabel("States")
+plt.ylabel("Coefficients")
+plt.savefig("Coefficient_states.png")
+plt.show()
+# print(model.score(xtest, ytest))
