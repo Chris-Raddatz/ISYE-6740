213e4ec004258cbdb84406333abc0da030f56339
Chris Rad <raddatzchris@gmail.com>
2024-04-02 11:27:41 -0500

    Trying some different things

--- linear regression.py
+++ linear regression.py
@@ -4,6 +4,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+from sklearn.metrics import mean_squared_error, r2_score
 
 np.random.seed(21)
 
@@ -11,17 +12,28 @@ file = pd.read_csv("data/merged_df.csv")
 
 states = list(file.columns)[4:]
 
-model = LinearRegression(fit_intercept= False, n_jobs = -1)
 x = file[['CPI']]
 y = file[states]
 
-xtrain, xtest, ytrain, ytest = train_test_split(x, y, test_size = 0.15, train_size = .85, random_state = 21)
+# params = {"fit_intercept" : [True, False], "n_jobs" : [None, 1, -1], "fit_intercept" : [True, False],
+#            "positive" : [True, False]}
+
+# model = LinearRegression()
+
+# gridsearch = GridSearchCV(model, param_grid= params)
+
+# gridsearch.fit(x, y)
+# print(gridsearch.best_params_)
+
+
+xtrain, xtest, ytrain, ytest = train_test_split(x, y, test_size = 0.20, train_size = .80, random_state = 21)
+model = LinearRegression(fit_intercept=False, n_jobs=None, positive=False)
 model.fit(xtrain, ytrain)
+y_pred = model.predict(xtest)
 
 cdf = pd.DataFrame(model.coef_, states, columns=['Coefficients'])
 
 ordered_by_coef = cdf.sort_values(by = "Coefficients", ascending = False)
-print(ordered_by_coef)
 
 plt.figure(figsize = (14,8))
 sns.barplot(ordered_by_coef.T)
@@ -31,4 +43,6 @@ plt.xlabel("States")
 plt.ylabel("Coefficients")
 plt.savefig("Visuals/Coefficient_states.png")
 plt.show()
-print(model.score(xtest, ytest))
+print("Model Training Score: {:.2f}".format(model.score(xtrain, ytrain)))
+print("Model Test Score: {:.2f}".format(model.score(xtest, ytest)))
+print("MSE {:.2f}".format(mean_squared_error(ytest, y_pred, squared = False)))
